753cbf374887f600ce03a5008795dac1f6b3e8f4
Mark Azemoon <mmazemoon@gmail.com>
2015-06-09 16:29:35 -0700

    Add authentication behavior to app controller and user route resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :ensure_logged_in
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -23,4 +23,9 @@ class ApplicationController < ActionController::Base
     session[:session_token] = nil
   end
 
+  def ensure_logged_in
+    unless logged_in?
+      redirect_to new_session_url
+    end
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+end
