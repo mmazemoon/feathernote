b881e8362e83fd75361fcb578e8ab316dfa6667d
Mark Azemoon <mmazemoon@gmail.com>
2015-06-09 15:30:25 -0700

    Add user authentication behaviors to model and application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,43 @@
 class User < ActiveRecord::Base
+  validates :email, :session_token, :password_digest, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64(16)
+    while User.find_by(session_token: token)
+      token = SecureRandom.urlsafe_base64(16)
+    end
+    token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token= User.generate_session_token
+    self.save!
+    session_token
+  end
+
 end
