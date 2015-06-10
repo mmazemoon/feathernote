6027234135b08743452ae1d22975e5f6ca481d84
Mark Azemoon <mmazemoon@gmail.com>
2015-06-10 14:43:47 -0700

    Install backbone

---
+++ app/assets/javascripts/feather_note.js
@@ -0,0 +1,13 @@
+window.FeatherNote = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  FeatherNote.initialize();
+});
