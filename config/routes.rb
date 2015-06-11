7746e2ee911bfa89a9e5ce90beae9f1f57abe707
Mark Azemoon <mmazemoon@gmail.com>
2015-06-11 11:00:39 -0700

    Add note and notebook resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  namespace :api, defaults: {format: :json} do |variable|
+    resources :notes, only: [:create, :show, :update, :delete, :index]
+    resources :notebooks, only: [:create, :show, :update, :delete, :index]
+  end
 end
