c6344ce62246dec966b2762ade169d8639d90967
Mark Azemoon <mmazemoon@gmail.com>
2015-06-11 13:42:35 -0700

    Improve initialize function to increase responsiveness

--- app/assets/javascripts/feather_note.js
+++ app/assets/javascripts/feather_note.js
@@ -3,17 +3,25 @@ window.FeatherNote = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
+  initialize: function(options) {
+    FeatherNote.currentUser = options.currentUser;
+
     FeatherNote.notes = new FeatherNote.Collections.notes();
     FeatherNote.notebooks = new FeatherNote.Collections.notebooks();
     FeatherNote.notes.fetch( {reset: true} );
     FeatherNote.notebooks.fetch( {reset: true} );
 
-    new FeatherNote.Routers.router( {$rootEl: $("main")} );
+    var $searchbar = $("#searchbar");
+    var $navbar = $("#navbar");
+    new FeatherNote.Routers.Router( {$rootEl: $("#content")} );
+
+    var navbarView = new FeatherNote.Views.navbarView();
+    var searchbarView = new FeatherNote.Views.searchbarView();
+    var sidebarView = new FeatherNote.Views.sidebarView();
+    $navbar.html(navbarView.render().$el);
+    $searchbar.html(searchbarView.render().$el);
+    $sidebar.html(sidebarView.render().$el);
+
     Backbone.history.start();
   }
 };
-
-$(document).ready(function(){
-  FeatherNote.initialize();
-});
