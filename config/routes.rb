3db9dcabb1b782d823a2e5e83ca5d62f97a971e3
Mark Azemoon <mmazemoon@gmail.com>
2015-06-11 13:43:45 -0700

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  namespace :api, defaults: {format: :json} do 
+  namespace :api, defaults: { format: :json } do 
     resources :notes, only: [:create, :show, :update, :delete, :index]
     resources :notebooks, only: [:create, :show, :update, :delete, :index]
   end
