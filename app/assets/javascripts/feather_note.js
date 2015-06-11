c29dadf1ffecc759c32faf7459c611196e8dc543
Mark Azemoon <mmazemoon@gmail.com>
2015-06-11 10:26:48 -0700

    Initialize feathernote with notebooks and notes

--- app/assets/javascripts/feather_note.js
+++ app/assets/javascripts/feather_note.js
@@ -4,7 +4,13 @@ window.FeatherNote = {
   Views: {},
   Routers: {},
   initialize: function() {
-    
+    FeatherNote.notes = new FeatherNote.Collections.notes();
+    FeatherNote.notebooks = new FeatherNote.Collections.notebooks();
+    FeatherNote.notes.fetch( {reset: true} );
+    FeatherNote.notebooks.fetch( {reset: true} );
+
+    new FeatherNote.Routers.router( {$rootEl: $("main")} );
+    Backbone.history.start();
   }
 };
 
