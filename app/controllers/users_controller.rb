6193c44571fbb209821cf908f726e55d1bb2056e
Mark Azemoon <mmazemoon@gmail.com>
2015-06-10 18:38:41 -0700

    Redirect new users to root_url after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login!(@user)
-      redirect_to
+      redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
