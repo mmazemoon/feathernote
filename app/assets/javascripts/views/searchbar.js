b6ed119161e299cf622cce27999094b0bf107b8d
Mark Azemoon <mmazemoon@gmail.com>
2015-06-11 22:10:56 -0700

    Add create note button

--- app/assets/javascripts/views/searchbar.js
+++ app/assets/javascripts/views/searchbar.js
@@ -2,8 +2,17 @@ FeatherNote.Views.Searchbar = Backbone.View.extend({
 
   template: JST['static_pages/searchbar'],
 
+  events: {
+    "submit #add-note": "createNote"
+  },
+
   render: function(){
     this.$el.html(this.template());
     return this;
+  },
+
+  createNote: function(event){
+    event.preventDefault();
   }
+
 });
