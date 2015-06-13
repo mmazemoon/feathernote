10bbd03d8404f0d282c0407207448ef77da809d3
Mark Azemoon <mmazemoon@gmail.com>
2015-06-12 17:25:53 -0700

    Include Backbone.CompositeView

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require jquery.serializejson.min
 //= require feather_note
 //= require_tree ../templates
+//= require_tree ./utils
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
