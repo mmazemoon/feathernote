22964080e25d3b54c126edbd57bdeac0e274f298
Mark Azemoon <mmazemoon@gmail.com>
2015-06-17 23:55:04 -0700

    Remove composite view

--- app/assets/javascripts/views/notebooks/notebook_show.js
+++ app/assets/javascripts/views/notebooks/notebook_show.js
@@ -1,29 +1,19 @@
-FeatherNote.Views.NotebookShow = Backbone.CompositeView.extend({
-
-  events: {
-    "click li.notebook-name-sidebar": "addMyView"
-  },
-
-  template: JST['notebooks/show'],
-
-  initialize: function(){
-    this.addSubview(".notes-list-content", this.subView());
-    this.listenTo(this.model, "sync", this.render);
-  },
-
-  render: function(){
-    var content = this.template;
-    this.$el.html(content);
-    this.attachSubviews();
-    return this;
-  },
-
-  subView: function(){
-    if(this._subView){
-      this.removeSubview(".notes-list-content", this._subView);
-    }
-    this._subView = new FeatherNote.Views.NotesIndex({ collection: this.model.notes() });
-    return this._subView; // an object
-  }
-
-});
+// FeatherNote.Views.NotebookShow = Backbone.CompositeView.extend({
+//
+//   events: {
+//     "click li.notebook-name-sidebar": "addMyView"
+//   },
+//
+//   template: JST['notebooks/show'],
+//
+//   initialize: function(){
+//     this.listenTo(this.model, "sync", this.render);
+//   },
+//
+//   render: function(){
+//     var content = this.template(notes = this.model.notes());
+//     this.$el.html(content);
+//     return this;
+//   }
+//
+// });
