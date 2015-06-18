341b0842e10893800b05d6a2c4615b6ec4e453b4
Mark Azemoon <mmazemoon@gmail.com>
2015-06-17 23:47:20 -0700

    Require active notebook and active note to be set

--- app/assets/javascripts/feather_note.js
+++ app/assets/javascripts/feather_note.js
@@ -5,21 +5,24 @@ window.FeatherNote = {
   Routers: {},
   initialize: function(options) {
     FeatherNote.currentUser = options.currentUser;
-    FeatherNote.notes = new FeatherNote.Collections.Notes();
-    FeatherNote.notebooks = new FeatherNote.Collections.Notebooks();
+    FeatherNote.notes = new FeatherNote.Collections.Notes({
+      success: function(){ FeatherNote.activeNote = FeatherNote.notes.first();}.bind(this)
+    });
+    FeatherNote.notebooks = new FeatherNote.Collections.Notebooks({
+      success: function(){FeatherNote.activeNotebook = FeatherNote.notebooks.first();}.bind(this)
+    });
 
     new FeatherNote.Routers.Router( {$rootEl: $(".content")} );
 
     var $navbar = $("#navbar");
-    var $searchbar = $(".searchbar");
+    var $searchbar = $("#searchbar");
     var $sidebar = $(".sidebar");
 
     FeatherNote.notebooks.fetch({
       success: function (){
-        var notebooksIndex = new FeatherNote.Views.NotebooksIndex({collection: FeatherNote.notebooks});
+        var notebooksIndex = new FeatherNote.Views.NotebooksIndex({ collection: FeatherNote.notebooks });
         $sidebar.html(notebooksIndex.render().$el);
-      },
-      reset: true });
+      }, reset: true });
 
     var navbar = new FeatherNote.Views.Navbar();
     var searchbar = new FeatherNote.Views.Searchbar();
